157210547666f001dbfb6092ebf8a2b653d183e9
eljeh <edbastarache@gmail.com>
2019-03-07 05:55:42 -0700

    Raising and Handling Events

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -10,10 +10,16 @@ class Counters extends Component {
 			{ id: 4, value: 0 },
 		]
 	}
+
+	handleDelete = () => {
+		console.log('Event Handle Called');
+	}
 	render() {
 		return (
 			<div className="mt-3 mx-5">
-				{this.state.counters.map(counter => <Counter key={counter.id} value={counter.value} id={counter.id} />)}
+				{this.state.counters.map(counter => (
+					<Counter key={counter.id} onDelete={this.handleDelete} value={counter.value} id={counter.id} />
+				))}
 			</div>
 		);
 	}
