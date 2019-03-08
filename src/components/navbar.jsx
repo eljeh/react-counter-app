5966c79c003f204fea7b1300f6fdcc791e6f2c4b
eljeh <edbastarache@gmail.com>
2019-03-08 05:01:23 -0700

    Multiple Components in Sync

---
+++ src/components/navbar.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class NavBar extends Component {
+	render() { 
+		return (
+			<div className="navbar navbar-light bg-light">
+				<a className="navbar-brand" href="#">
+					NavBar
+				</a>
+			</div>
+		  );
+	}
+}
+ 
+export default NavBar;
